12cb1320ae4ac353bd749ad48fd122c147591f70
H1R0369 <161938850+H1R0369@users.noreply.github.com>
2025-08-23 11:10:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 document.getElementsByClassName('hamburger-btn')[0].addEventListener('click', () => {
-    document.getElementsByClassName('dropdown-menu')[0].style.display = 'flex';
+    setTimeout(() => {
+        document.getElementsByClassName('dropdown-menu')[0].style.display = 'flex';
+    }, 200);
 });
 
 document.getElementsByClassName('home-btn')[0].addEventListener('click', () => {
-    document.getElementsByClassName('dropdown-menu')[0].style.display = 'none';
-})
+    setTimeout(() => {
+        document.getElementsByClassName('dropdown-menu')[0].style.display = 'none';
+    }, 200);
+});
+
+if (window.innerWidth < 1024) {
+
+    let clickables = document.querySelectorAll('.clickable');
+
+    for (let i = 0; i < clickables.length; i++) {
+        clickables[i].addEventListener('click', () => {
+            clickables[i].style.transition = "0.5s";
+            clickables[i].style.transform = "scale(1.2, 1.2)"
+            setTimeout(() => {
+                clickables[i].style.transform = "scale(1, 1)"
+            }, 200);
+        })
+    }
+}
