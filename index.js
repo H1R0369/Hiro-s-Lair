2c8d73b5c623a50f1c92413fa2f5740157ca28d5
H1R0369 <161938850+H1R0369@users.noreply.github.com>
2025-08-23 13:20:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-document.getElementsByClassName('hamburger-btn')[0].addEventListener('click', () => {
-    setTimeout(() => {
-        document.getElementsByClassName('dropdown-menu')[0].style.display = 'flex';
-    }, 200);
-});
+let dropdown = document.getElementsByClassName('dropdown-menu-container')[0];
+let homeBtns = document.getElementsByClassName('home')
+let clickables = document.querySelectorAll('.clickable');
 
-document.getElementsByClassName('home-btn')[0].addEventListener('click', () => {
-    setTimeout(() => {
-        document.getElementsByClassName('dropdown-menu')[0].style.display = 'none';
-    }, 200);
-});
+document.getElementsByClassName('hamburger-btn')[0].addEventListener('click', () => {transitionMenu('open')});
+
+for (let i = 0; i < homeBtns.length; i++) {
+        document.getElementsByClassName('home')[i].addEventListener('click', () => {transitionMenu('close')});
+};
 
 if (window.innerWidth < 1024) {
-
-    let clickables = document.querySelectorAll('.clickable');
-
     for (let i = 0; i < clickables.length; i++) {
         clickables[i].addEventListener('click', () => {
-            clickables[i].style.transition = "0.5s";
-            clickables[i].style.transform = "scale(1.2, 1.2)"
+            clickables[i].style.transform = 'scale(1.2, 1.2)';
             setTimeout(() => {
-                clickables[i].style.transform = "scale(1, 1)"
+                clickables[i].style.transform = 'scale(1, 1)';
             }, 200);
-        })
-    }
+        });
+    };
 }
+
+function transitionMenu(action) {
+    let xTranslation = action == 'open' ? '0' : '100%';
+    let delay = innerWidth < 1024 ? 200 : 0;
+    setTimeout(() => {
+        dropdown.style.transform = `translate(${xTranslation}, 0)`;
+    }, delay);
+};
